985f11f030dd33df1024069dc2685020891a2151
sandrow <sandrow.townsend@gmail.com>
2014-04-23 17:55:41 -0400

    final assignment

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+function cycleImages(){
+      var $active = $('#cycler .active');
+      var $next = ($active.next().length > 0) ? $active.next() : $('#cycler img:first');
+      $next.css('z-index',2);
+      $active.fadeOut(1500,function(){
+	  $active.css('z-index',1).show().removeClass('active');
+          $next.css('z-index',3).addClass('active');
+      });
+    }
+
+$(document).ready(function(){
+
+setInterval('cycleImages()', 4000);
+})
+
+
+$(document).ready(function(){
+    $("#topnav a").click(function(){
+    var navId = $(this).attr("href");
+    $("body,html").animate({scrollTop: $(navId).offset().top}, "slow");
+    return false;
+   });
+
+});
+
+
+
